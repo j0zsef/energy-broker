c80ba9c035c396ea7f9d800ff4f2334647ed92b3
Joe Nevin <jnevin@enova.com>
2025-02-04 15:09:58 -0600

    Removing tmp & dist.

--- dist/libs/backend/src/lib/backend.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.backend = backend;
-function backend() {
-    return 'backend';
-}
-//# sourceMappingURL=backend.js.map
--- dist/libs/shared/src/lib/shared.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.shared = shared;
-function shared() {
-    return 'shared';
-}
-//# sourceMappingURL=shared.js.map
