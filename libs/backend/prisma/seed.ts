6f3145f9bb81174816f622b4848f0fadd5cee339
Joe Nevin <jnevin@enova.com>
2025-08-21 14:02:37 -0500

    Seeds WIP.

---
+++ libs/backend/prisma/seed.ts
@@ -0,0 +1,24 @@
+import { prismaClient } from "../src";
+
+async function main() {
+  const mockUtil = await prismaClient.oAuthProviderConfig.upsert({
+      where: { providerName: 'mock-util' },
+      update: {},
+      create: {
+        authUrl: 'http://localhost:3001/authorize',
+        providerName: "mock-util",
+        tokenUrl: 'http://localhost:3001/token',
+        redirectUri: "http://localhost:4200/sources"
+      }
+  })
+  console.log({ mockUtil })
+}
+main()
+  .then(async () => {
+    await prismaClient.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prismaClient.$disconnect()
+    process.exit(1)
+  })
